1034176530db048b1f48870697f6d56d339fc0a2
gengbing <edwingeng@126.com>
2018-07-01 17:48:13 +0800

    add some benchmarks

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # Overview
 This is a variation of the Google's jump consistent hash. It overcomes the shortcoming of not being able to remove nodes of the original implementation at the price of not being so consistent under some circumstances.
 
+# Benchmark
+```
+BenchmarkDoubleJump/10-nodes            20000000            73.3 ns/op
+BenchmarkDoubleJump/100-nodes           20000000            85.9 ns/op
+BenchmarkDoubleJump/1000-nodes          20000000            97.5 ns/op
+
+BenchmarkStathatConsistent/10-nodes      5000000           293 ns/op
+BenchmarkStathatConsistent/100-nodes     5000000           327 ns/op
+BenchmarkStathatConsistent/1000-nodes    3000000           435 ns/op
+
+BenchmarkSerialxHashring/10-nodes        5000000           283 ns/op
+BenchmarkSerialxHashring/100-nodes       5000000           341 ns/op
+BenchmarkSerialxHashring/1000-nodes      3000000           426 ns/op
+```
+
 # Example
 ```
 h := NewHash()
---
+++ benchmark/benchmark_test.go
@@ -0,0 +1,68 @@
+package benchmark
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/edwingeng/doublejump"
+	"github.com/serialx/hashring"
+	"stathat.com/c/consistent"
+)
+
+func BenchmarkDoubleJump(b *testing.B) {
+	for i := 10; i <= 1000; i *= 10 {
+		b.Run(fmt.Sprintf("%d-nodes", i), func(b *testing.B) {
+			h := doublejump.NewHash()
+			for j := 0; j < i; j++ {
+				h.Add(fmt.Sprintf("node%d", j))
+			}
+
+			b.ResetTimer()
+			for j := 0; j < b.N; j++ {
+				h.Get(uint64(j))
+			}
+		})
+	}
+}
+
+func BenchmarkStathatConsistent(b *testing.B) {
+	for i := 10; i <= 1000; i *= 10 {
+		b.Run(fmt.Sprintf("%d-nodes", i), func(b *testing.B) {
+			h := consistent.New()
+			for j := 0; j < i; j++ {
+				h.Add(fmt.Sprintf("node%d", j))
+			}
+			a := make([]string, b.N)
+			for j := 0; j < b.N; j++ {
+				a[j] = strconv.Itoa(j)
+			}
+
+			b.ResetTimer()
+			for j := 0; j < b.N; j++ {
+				h.Get(a[j])
+			}
+		})
+	}
+}
+
+func BenchmarkSerialxHashring(b *testing.B) {
+	for i := 10; i <= 1000; i *= 10 {
+		b.Run(fmt.Sprintf("%d-nodes", i), func(b *testing.B) {
+			nodes := make([]string, i)
+			for j := 0; j < i; j++ {
+				nodes[j] = fmt.Sprintf("node%d", j)
+			}
+			h := hashring.New(nodes)
+			a := make([]string, b.N)
+			for j := 0; j < b.N; j++ {
+				a[j] = strconv.Itoa(j)
+			}
+
+			b.ResetTimer()
+			for j := 0; j < b.N; j++ {
+				h.GetNode(a[j])
+			}
+		})
+	}
+}
